32d978d5c53f5c9cb5e2e176eadca89c309f94e7
Scott Schumacker <scottschu97@gmail.com>
2025-02-21 23:53:44 -0500

    updated code to create test data set

--- data_creation.R
+++ data_creation.R
@@ -1,2 +1,29 @@
 # Scott Schumacker
 # Data set Creation
+
+# Loading packages
+library(caret)
+library(ggplot2)
+library(dplyr)
+
+# Creating data set
+# Creating patients
+n <- 100
+
+# Creating age and cholesterol data
+age <- sample(30:80, n, replace = TRUE)
+cholesterol <- rnorm(n, mean = 200, sd = 30)
+
+# Creating true coefficients
+intercept <- 90
+age_coef <- 0.5
+cholesterol_coef <- 0.1
+
+# Generating error term
+error <- rnorm(n, mean = 0, sd = 10)
+
+# Generating response variable
+systolic_bp <- intercept + age_coef * age + cholesterol_coef * cholesterol + error
+
+# Creating data frame
+bp_data <- data.frame(age = age, cholesterol = cholesterol, systolic_bp = systolic_bp)
